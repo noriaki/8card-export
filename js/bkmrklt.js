d7e28f50f8b08a554b157b3177a7664fb2233e52
Noriaki UCHIYAMA <uchiyama.noriaki@gmail.com>
2014-03-22 21:19:19 +0900

    fixed typo path

--- js/bkmrklt.js
+++ js/bkmrklt.js
@@ -1,5 +1,5 @@
 jQuery(function($) {
-  var uri = 'https://github.com/noriaki/8card-export/raw/0.1.0/export8card.js';
+  var uri = 'https://github.com/noriaki/8card-export/raw/0.1.0/js/export8card.js';
   var deferred = $.Deferred();
   deferred
     .then(function() {
